60b46d69e4d0feb678a5f05eaef1ba99c2b981e6
Tateo <91488652+TateoDev@users.noreply.github.com>
2021-10-21 10:19:17 -0600

    Create MLA Header.py
    This doesn't save much time but ¯\_(ツ)_/¯

---
+++ MLA Header.py
@@ -0,0 +1,25 @@
+from datetime import date
+
+today = date.today().strftime("%d/%b/%Y")
+
+name = input("enter your name \n")
+teacher = input("enter your teacher's name \n")
+course = input("enter your course \n")
+
+complete = f"{name} \n {teacher} \n {course} \n {today}"
+
+print("#"*10)
+print(complete)
+print("#"*10)from datetime import date
+
+today = date.today().strftime("%d/%b/%Y")
+
+name = input("enter your name \n")
+teacher = input("enter your teacher's name \n")
+course = input("enter your course \n")
+
+complete = f"{name} \n {teacher} \n {course} \n {today}"
+
+print("#"*10)
+print(complete)
+print("#"*10)
